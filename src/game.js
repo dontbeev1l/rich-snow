1f58e3ed093d39f7960e89ebfc33ef56786100b5
dontbeev1l <42718214+dontbeev1l@users.noreply.github.com>
2022-06-13 11:04:24 +0300

    game

--- src/game.js
+++ src/game.js
@@ -1,37 +1,41 @@
-MLib.debug = true;
+// MLib.debug = true;
 
-const app = new PIXI.Application()
-document.getElementById('container').appendChild(app.view);
+// const TEXTURES = {
+//     background: '/images/back.png',
+// }
 
-const sprite = PIXI.Sprite.from('../images/1.png');
+// const app = new PIXI.Application()
+// document.getElementById('container').appendChild(app.view);
 
-
-sprite.width = 1920;
-sprite.height = 1080;
-
-app.stage.addChild(sprite);
+// const sprite = PIXI.Sprite.from('../images/1.png');
 
 
-const scale = new MLib.Scale(
-    document.getElementById('container'),
-    app.view,
-    {
-        width: 1920,
-        height: 1080
-    });
+// sprite.width = 1920;
+// sprite.height = 1080;
 
-scale.addResizeListener((e) => {
-    app.renderer.resize(e.width, e.height);
-    console.log(e);
-})
-
-scale.addResizeListener((e) => {
-    const coords = scale.transformCoords({ x: 0, y: 0 });
-    const size = scale.transformSize({ width: 1920, height: 1080 });
+// app.stage.addChild(sprite);
 
 
-    sprite.width = size.width;
-    sprite.height = size.height;
-    sprite.x = coords.x;
-    sprite.y = coords.y;
-})
+// const scale = new MLib.Scale(
+//     document.getElementById('container'),
+//     app.view,
+//     {
+//         width: 1920,
+//         height: 1080
+//     });
+
+// scale.addResizeListener((e) => {
+//     app.renderer.resize(e.width, e.height);
+//     console.log(e);
+// })
+
+// scale.addResizeListener((e) => {
+//     const coords = scale.transformCoords({ x: 0, y: 0 });
+//     const size = scale.transformSize({ width: 1920, height: 1080 });
+
+
+//     sprite.width = size.width;
+//     sprite.height = size.height;
+//     sprite.x = coords.x;
+//     sprite.y = coords.y;
+// })
